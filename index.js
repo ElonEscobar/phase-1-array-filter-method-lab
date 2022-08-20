a25bc2eec5910b08bc48963f35a1917a13076af8
Dan Muendo <danwalker254254@gmail.com>
2022-08-20 10:47:55 +0100

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(someArray, query) {
+    return someArray.filter((name) => name.toLowerCase().includes(query.toLowerCase()));
+}
+function fuzzyMatch(someArray, query){
+    const driverFilter = someArray.filter(each => each.startsWith(query))
+    if (driverFilter){
+        return driverFilter;
+    }
+    return []
+}
+
+function matchName(someArray, query){
+    const match = someArray.filter(each => each.name === query);
+    return match
+}
